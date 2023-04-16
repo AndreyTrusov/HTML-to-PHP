b4fea2cb661e4a29f8935ae2f4d253f6f6ce4f0f
unknown <trusovandrey.aleks@gmail.com>
2023-04-16 15:03:22 +0200

    cosmetic changes +delete some JS

--- partials/header.php
+++ partials/header.php
@@ -1,5 +1,4 @@
 <head>
-
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="">
@@ -13,9 +12,7 @@
   <!-- Bootstrap core CSS -->
   <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-
   <!-- Additional CSS Files -->
-  <!-- <link rel="stylesheet" href="assets/css/fontawesome.css"> -->
   <link rel="stylesheet" href="assets/css/templatemo-stand-blog.css">
   <link rel="stylesheet" href="assets/css/owl.css">
   
